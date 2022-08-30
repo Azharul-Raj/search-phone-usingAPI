d2b91b23abdfcacb9360cb7101c740d51c005d92
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-08-30 09:28:16 +0600

    search functionalitis added

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const loadPhone = async () => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+const loadPhone = async (name) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${name}`)
     const data = await res.json()
-    console.log(data)
+    fullPhone(data.data)
 
 }
-loadPhone()
+
+const fullPhone = (phones) => {
+    const displayPhone = document.getElementById('display-phone')
+    console.log(phones)
+    phones.forEach(phone => {
+        const {brand, phone_name, image}=phone
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card h-100 p-3">
+            <img src="${image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${phone_name}</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `
+        displayPhone.appendChild(div)
+    })
+}
+// add click event handler
+document.getElementById('search-btn').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value 
+    searchField.value = ``
+    loadPhone(searchText)
+})
+
+
+
+// loadPhone('a')
